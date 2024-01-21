b2e2240327076be89b7e2cffb34750b237fbf423
AdelSamy00 <adelsamy188@gmail.com>
2024-01-21 18:23:46 +0200

    add some fake data to front-end

--- server/models/Review.js
+++ server/models/Review.js
@@ -4,6 +4,11 @@ const reviewSchema = new mongoose.Schema(
     customer: { type: Schema.Types.ObjectId, ref: 'Customers' },
     product: { type: Schema.Types.ObjectId, ref: 'Products' },
     comment: { type: String, required: true },
+    rating: {
+      type: Number,
+      minlength: [1, 'Rating length should be greater than 1'],
+      maxlength: [5, 'Rating length should be less than 5'],
+    },
   },
   {
     timestamps: true,
