d3259b63bac141b579ff1cfd3b0faf2c0752df3b
AdelSamy00 <adelsamy188@gmail.com>
2024-01-22 17:43:50 +0200

    add tailwaind and add landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile.jsx';
-
-
+import { Landing } from './pages/';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Landing />}></Route>
       <Route path="/profile" element={<Profile />}></Route>
     </Routes>
   );
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './StyleSheets/Landing.css';
+import { Link, NavLink } from 'react-router-dom';
+function Landing() {
+  return (
+    <div className="landding flex text-center text-white bg-dark">
+      <div className=" w-full flex w-100 h-100 p-3 mx-auto flex-col">
+        <header className="mb-auto">
+          <div className="">
+            <h3 className="md:float-left mb-0">Smart Vision</h3>
+            <nav className="nav nav-masthead justify-center md:float-right">
+              <NavLink
+                to={'/'}
+                className="nav-link active"
+                aria-current="pages"
+              >
+                Home
+              </NavLink>
+              <NavLink to={'/login'} className="nav-link">
+                Login
+              </NavLink>
+              <NavLink to={'/register'} className="nav-link">
+                Register
+              </NavLink>
+            </nav>
+          </div>
+        </header>
+        <main className="px-3">
+          <h1>Smart Vision</h1>
+          <p className="lead">
+            Welcome to Smart Vision! <br /> Jump right in and explore our many
+            products. <br />
+            Feel free to share some of your own and comment on others!
+          </p>
+          <a
+            href="/"
+            className="btn btn-lg btn-secondary font-bold  text-black"
+          >
+            View Product
+          </a>
+        </main>
+
+        <footer className="mt-auto text-white-50">
+          <p>&copy; 2024 </p>
+        </footer>
+      </div>
+
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I"
+        crossorigin="anonymous"
+      />
+    </div>
+  );
+}
+
+export default Landing;
