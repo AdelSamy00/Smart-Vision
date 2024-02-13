9dcbe9609d8c42e9d5fbeb1714e62e0fd3a222c7
Demiana_Ebrahim <96248453+DemianaEbrahim@users.noreply.github.com>
2024-02-13 10:34:41 +0200

    modification at DeleteAccount

--- client/src/pages/DeleteAccountPage.jsx
+++ client/src/pages/DeleteAccountPage.jsx
@@ -19,6 +19,8 @@ const DeleteAccountPage = () => {
 
       if (response.status === 200) {
         setIsAccountDeleted(true);
+      } else if (response.status === 401) {
+        setError('Incorrect password. Please try again.');
       } else {
         console.error('Failed to delete account', response.data);
         setError(response.data?.message || 'Failed to delete account');
@@ -34,8 +36,8 @@ const DeleteAccountPage = () => {
       {!isAccountDeleted ? (
         <DeleteAccount onDelete={handleDelete} error={error} />
       ) : (
-        <div className="account-deleted-message">
-          <h2>Your account is deleted</h2>
+        <div className="account-deleted-message" style={{fontSize:"20px"}}>
+          <h2 style={{fontWeight:"bold"}} >Your account is deleted</h2>
           <p>Your Smart Vision account has now been deleted.</p>
           <p>We are sad to see you leave, and hope you will come back in the future. You can continue to browse the site if you wish.</p>
         </div>
