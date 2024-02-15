121af3048f4089566c82ed55470a4b3b3d8278ee
MarinaWilliamMaher <marinawilliam210@gmail.com>
2024-02-16 01:40:05 +0200

    Edit in history and home

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -3,15 +3,20 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import OrderComponent from "../components/OrderComponent";
 import ServiceHistory from "../components/serviceHistory";
+import Loading from "../components/Loading";
+import { Button, FormControlLabel, Switch } from "@mui/material";
 const History = () => {
   const [orderHistory, setOrderHistory] = useState([]);
   const { customer } = useSelector((state) => state.customer);
+  const [showOrderHistory, setShowOrderHistory] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchOrderHistory() {
       try {
         const response = await axios.get(`/customers/order/${customer._id}`);
         setOrderHistory(response.data.history);
+        setIsLoading(false);
         console.log(response.data);
       } catch (error) {
         console.error(
@@ -23,21 +28,47 @@ const History = () => {
 
     fetchOrderHistory();
   }, []);
+  const toggleHistory = () => {
+    setShowOrderHistory((prevShowOrderHistory) => !prevShowOrderHistory);
+  };
   return (
     <div>
       {/* <h1>Order History</h1> */}
-      <ul>
-        <li>
-          <h1 style={{fontWeight:"bold",fontSize:"20px",marginLeft:"20px"}}>Order History</h1>
-          {orderHistory.map((order, index) => (
-            <OrderComponent key={index} order={order} />
-          ))}
-        </li>
-        <li>
-          <h1 style={{fontWeight:"bold",fontSize:"20px",marginLeft:"20px"}}>Service History</h1>
-          <ServiceHistory />
-        </li>
-      </ul>
+      <FormControlLabel
+        labelPlacement="start"
+        label={showOrderHistory ? "Order History" : "Service History"}
+        control={
+          <Switch
+            checked={showOrderHistory}
+            onChange={toggleHistory}
+            color="primary"
+          />
+        }
+        sx={{ marginBottom: "1rem",marginLeft:"5%" }}
+      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <ul>
+          {showOrderHistory ? (
+            <li>
+              {/* <h1 style={{fontWeight:"bold",fontSize:"20px",marginLeft:"20px"}}>Order History</h1> */}
+              {orderHistory.length > 0 ? (
+                orderHistory.map((order, index) => (
+                  <OrderComponent key={index} order={order} />
+                ))
+              ) : (
+                <p>You haven't ordered yet.</p>
+              )}
+            </li>
+          ) : (
+            <li>
+              {/* <h1 style={{fontWeight:"bold",fontSize:"20px",marginLeft:"20px"}}>Service History</h1> */}
+              <ServiceHistory />
+            </li>
+          )}
+        </ul>
+      )}
     </div>
   );
 };
