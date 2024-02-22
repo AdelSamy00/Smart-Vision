2a49133a9f4ebc285bef530e2d32149ee60d5fbc
youssefayad23 <119113349+youssefayad23@users.noreply.github.com>
2024-02-22 13:55:58 +0200

    Add cart slice to redux

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import CustomerSlice from './CustomerSlice.js';
+import CartSlice from './CartSlice.js';
 
 const rootReducer = combineReducers({
   customer: CustomerSlice,
+  cart: CartSlice,
 });
 
 export { rootReducer };
